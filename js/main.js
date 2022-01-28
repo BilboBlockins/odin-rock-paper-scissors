7394aed7b302f2102c0c1f23819f30945ca2a2cb
brett <abretthiebert@gmail.com>
2022-01-28 14:10:04 -0500

    added rock hand favicon

--- js/main.js
+++ js/main.js
@@ -30,6 +30,7 @@ async function startGame() {
   changeIcon("rock", "rock");
 }
 
+//Checks for end game condition and gives message.
 function isGameOver() {
   if(roundNum > 5) {
     roundDisplay.innerText = `Game Over.`
@@ -40,16 +41,17 @@ function isGameOver() {
     } else {
   	  readout.innerText = `Best of 5 rounds is a tie.`;
     }
-    roundNum = 1;
-    computerWins = 0;
-    playerWins = 0;
     return true;
   } else {
   	return false;
   }
 }
 
+//Resets all the readouts, round number, and score 
 function handlePlayAgain() {
+  roundNum = 1;
+  computerWins = 0;
+  playerWins = 0;
   console.log("Player wants to play again.");
   changeIcon("rock", "rock");
   playAgainBtn.className = "choice-btn hide";
@@ -63,6 +65,7 @@ function handlePlayAgain() {
   roundDisplay.innerText = "Round 1 of 5: Choose your weapon!";
 }
 
+//Adds some animation to the hands during play
 async function animatePlay(time) {
   rockBtn.disabled = true;
   paperBtn.disabled = true;
@@ -84,12 +87,13 @@ async function animatePlay(time) {
   scissorsBtn.disabled = false;
 }
 
+//Returns a promise to add delay to async functions
 function delayForBounce(time) {
-	return new Promise((resolve) => {
-		setTimeout(() => {
-			resolve();
-		}, time);	
-	});
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+	}, time);	
+  });
 }
 
 //Main function that handles choice clicks
@@ -100,45 +104,43 @@ async function handleChoice(choice) {
   computerHandText.innerText = computerChoice.toUpperCase();
 
   changeIcon(choice, computerChoice);
-
   console.log(`You chose ${choice}`);
   console.log(`Computer chose ${computerChoice}`);
-
   let outcome = playRound(choice, computerChoice);
   handleResults(outcome, choice, computerChoice);
 }
 
+//Changes icon based on selections (rock during play like in real life)
 function changeIcon(playerIcon, computerIcon) {
-	//replace all current classes with the updated versions
-	//scissors icon has a different rotation so needs its own condition
-	if(playerIcon !== 'scissors'){
-		playerHand.className = 
-		`fa-rotate-90 player-hand far fa-hand-${playerIcon} fa-7x`;
-	} else {
-		playerHand.className = 
-		`fa-flip-horizontal player-hand far fa-hand-${playerIcon} fa-7x`;
-	}
-
-	if(computerIcon !== 'scissors') {
-		computerHand.className = 
-		`flipper computer-hand far fa-hand-${computerIcon} fa-7x`;
-	} else {
-		computerHand.className = 
-		`computer-hand far fa-hand-${computerIcon} fa-7x`;	
-	}
+  //replace all current classes with the updated versions
+  //scissors icon has a different rotation so needs its own condition
+  if(playerIcon !== 'scissors'){
+    playerHand.className = 
+	`fa-rotate-90 player-hand far fa-hand-${playerIcon} fa-7x`;
+  } else {
+    playerHand.className = 
+	`fa-flip-horizontal player-hand far fa-hand-${playerIcon} fa-7x`;
+  }
+  if(computerIcon !== 'scissors') {
+    computerHand.className = 
+	`flipper computer-hand far fa-hand-${computerIcon} fa-7x`;
+  } else {
+    computerHand.className = 
+    `computer-hand far fa-hand-${computerIcon} fa-7x`;	
+  }
 }
 
-//Used with buttons to readout score and add to wins. todo: need to update icon classes.
+//Used with buttons to readout score and add to wins. Increments rounds.
 function handleResults(outcome, choice, computerChoice) {
   if (outcome === "Tie") {
     readout.innerText = `You tied with ${choice.toUpperCase()} - no points.`
     scoreboard.innerText = `Score: You - ${playerWins}, Computer - ${computerWins}`
   } else if (outcome === "Win") {
-    readout.innerText = `You Win! ${choice.toUpperCase()} beats ${computerChoice.toUpperCase()}!`
+    readout.innerText = `You Win!!! ${choice.toUpperCase()} beats ${computerChoice.toUpperCase()}!`
     playerWins++;
     scoreboard.innerText = `Score: You - ${playerWins}, Computer - ${computerWins}`
   } else if (outcome === "Lose") {
-    readout.innerText = `You Lose! ${computerChoice.toUpperCase()} beats ${choice.toUpperCase()}!`
+    readout.innerText = `You Lose... ${computerChoice.toUpperCase()} beats ${choice.toUpperCase()}.`
     computerWins++;
     scoreboard.innerText = `Score: You - ${playerWins}, Computer - ${computerWins}`
   }
@@ -157,93 +159,34 @@ function handleResults(outcome, choice, computerChoice) {
 
 //Handles logic for the round.
 function playRound(playerSelection, computerSelection) {
-
-	//check for tie condition
-	if (playerSelection === computerSelection) return "Tie";
-	//check other win conditions - since tie is covered already any non-win is a loss.
-	switch (playerSelection) {
-		case "rock":
-			return (computerSelection === "scissors") ? "Win" : "Lose";
-		case "paper":
-			return (computerSelection === "rock") ?  "Win" : "Lose";
-		case "scissors":
-			return (computerSelection === "paper") ? "Win" : "Lose";
-		default:
-			return "Someone won I'm just too dumb to know who...";	
+  //check for tie condition
+  if (playerSelection === computerSelection) return "Tie";
+  //check other win conditions - since tie is covered already any non-win is a loss.
+  switch (playerSelection) {
+    case "rock":
+      return (computerSelection === "scissors") ? "Win" : "Lose";
+	case "paper":
+      return (computerSelection === "rock") ?  "Win" : "Lose";
+	case "scissors":
+	  return (computerSelection === "paper") ? "Win" : "Lose";
+	default:
+	  return "Someone won I'm just too dumb to know who...";	
 	}
 }
 
 //Function returns random value for computer's play.
 function computerPlay() {
-    //Get random 0, 1, or 2
-    let num = Math.floor(Math.random() * 3);
-    //Return choice
-    switch (num) {
-    	case 0:
-    		return "rock";
-    	case 1:
-    		return "paper";
-    	case 2:
-    		return "scissors";
-    	default:
-    		return "wtf?";
+  //Get random 0, 1, or 2
+  let num = Math.floor(Math.random() * 3);
+  //Return choice
+  switch (num) {
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    case 2:
+      return "scissors";
+    default:
+      return "wtf?";
     }
 }
-
-//Fuction return player input (will lowercase all but better to use buttons later).
-function playerPlay() {
-	let input = prompt("Rock, Paper, or Scissors?");
-	return input.toLowerCase();
-}
-
-//Validation function for typed input - not needed with button input.
-function validChoice(choice) {
-	return (choice === "rock" || choice === "paper" || choice === "scissors")
-}
-
-//Game function to keep track of game
-function game() {
-	let round = 0;
-	let computerWin = 0;
-	let playerWin = 0;
-
-	//Main loop - play 5 rounds
-	while (round < 5) {
-		let computerChoice = computerPlay();
-		let choice = playerPlay();
-
-		//Some validation here since they are typing this in
-		while (!validChoice(choice)) {
-			alert('Invalid play - choose Rock, Paper, or Scissors.');
-			choice = playerPlay();
-		}
-
-		console.log(`You Chose ${choice}`);
-		console.log(`Computer Chose ${computerChoice}\n`)
-		
-		//Log the play output.
-		let outcome = playRound(choice, computerChoice)
-		
-		if (outcome === "Tie") {
-			console.log("You tied - no one wins points.")
-			console.log (`Score: You - ${playerWin}, Computer - ${computerWin}`)
-		} else if (outcome === "Win") {
-			console.log(`You Win! ${choice} beats ${computerChoice}!`)
-			playerWin++;
-			console.log (`Score: You - ${playerWin}, Computer - ${computerWin}`)
-		} else if (outcome === "Lose") {
-			console.log(`You Lose! ${choice} beats ${computerChoice}!`)
-			computerWin++;
-			console.log (`Score: You - ${playerWin}, Computer - ${computerWin}`)
-		}
-		
-		//Count round
-		round++;
-		
-		console.log(`End of round ${round}\n\n`);
-	}
-	
-}
-
-//Run game - change to event driven game by clicks
-//game();
